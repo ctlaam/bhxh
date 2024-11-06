9d7f0c20c2f4b38dcfd6adf2c1ced7744be0a40e
Your Name <your.email@example.com>
2024-11-06 10:41:59 +0700

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,7 +22,7 @@ export default async function ({ store, redirect, route }) {
   }
   // Nếu trạng thái isAuthenticated là false và route khác /auth và /auth/login
   if (
-    !isAuthenticated && route.path !== '/login/' && !route.path.includes('/my/rules') && !route.path === '/login/signup' && !route.path.includes('/login/signup/') && route.path !== '/'
+    !isAuthenticated && !route.path.includes('login') && !route.path.includes('signup') && route.path !== '/' && !route.path.includes('my/rules')
   ) {
     // Chuyển hướng đến /auth/signup
     return redirect('/login/')
