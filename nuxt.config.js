ea6e2834cd1934313d5613f9ace59ec899e69659
liam-dev <liam@gmail.com>
2025-06-11 15:50:36 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Viettel Post',
+    title: 'BESTBUY',
     htmlAttrs: {
       lang: 'en',
     },
