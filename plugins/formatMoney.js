587e437f7c6878e4f322b4d545b1e96871309bb8
liam-dev <liam@gmail.com>
2025-07-15 16:15:19 +0700

    1

--- plugins/formatMoney.js
+++ plugins/formatMoney.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 
 Vue.filter('formatVND', function (value) {
-  if (!value) return '0 $'
+  if (!value) return '0 VNĐ'
 
-  // Chuyển số thành chuỗi và thêm dấu phẩy
-  const formattedNumber = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  // Làm tròn số và ép kiểu về integer
+  const rounded = Math.round(Number(value))
+
+  // Chuyển số thành chuỗi và thêm dấu phẩy ngăn cách
+  const formattedNumber = rounded.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
   return formattedNumber + ' VNĐ'
 })
